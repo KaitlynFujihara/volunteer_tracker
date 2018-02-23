338266c69c8194ce258322ed8671ae4ba42f531e
Kaitlyn <katiefujihara@gmail.com>
2018-02-23 13:53:47 -0800

    got volunteers method to pass

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,6 @@
 class Project
   attr_accessor(:title)
-  attr_reader(:id)
+  attr_reader(:project_id, :id)
 
   def initialize(attributes)
     @title = attributes[:title]
@@ -12,6 +12,7 @@ class Project
     projects = []
     all_projects.each do |project|
       title = project.fetch("title")
+      project_id = project.fetch("id").to_i()
       id = project.fetch("id").to_i()
     projects.push(Project.new({:title => title, :id => id}))
     end
@@ -21,7 +22,19 @@ class Project
   def self.find(id)
     result = DB.exec("SELECT * FROM projects WHERE id = #{id};")
     title = result.first().fetch("title")
+    project_id = result.first().fetch("id")
     Project.new({:title => title, :id => id})
+  end
+
+  def volunteers
+    list_volunteers = []
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id()};")
+    volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      list_volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    list_volunteers
   end
 
   def save
