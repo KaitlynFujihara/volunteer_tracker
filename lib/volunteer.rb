689d32232e795506846a84ed4b9845a3ab4344fa
Kaitlyn <katiefujihara@gmail.com>
2018-02-23 09:33:11 -0800

    pass 3 specs in volunteer_spec

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,2 +1,18 @@
-class volunteers
-  attr_accessor(:name)
+class Volunteer
+  attr_accessor(:name, :project_id)
+  attr_reader(:id)
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @project_id = attributes[:project_id]
+  end
+
+  def self.all
+    all_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    all_volunteers.each do |volunteer|
+    volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+    end
+    volunteers
+  end
+end
